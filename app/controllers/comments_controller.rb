86d1732533a4aac8149a42ed5d80bd31dbabc938
Fabio Martins <fabio.martins@unila.edu.br>
2025-05-02 10:23:13 -0300

    feat(controller): add Comments Controller with default methods

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,38 @@
+class CommentsController < ApplicationController
+  before_action :set_task
+  before_action :set_comment, only: [ :destroy ]
+
+  def create
+    @comment = @task.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to @task }
+      end
+    else
+      redirect_to @task, alert: "Unable to add comment."
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to @task }
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:task_id])
+  end
+
+  def set_comment
+    @comment = @task.comments.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
