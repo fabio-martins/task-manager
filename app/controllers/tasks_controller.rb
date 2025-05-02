46fe5ef0e9157666472d7a158033b8429e5d5e64
Fabio Martins <fabio.martins@unila.edu.br>
2025-05-02 10:20:14 -0300

    feat(controller): add TasksController with default methods

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = current_user.tasks.build
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+
+    if @task.save
+      redirect_to tasks_path, notice: "Task was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to tasks_path, notice: "Task was successfully updated." }
+        format.turbo_stream { redirect_to tasks_path, notice: "Task was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.turbo_stream { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_url, notice: "Task was successfully deleted."
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :status, :due_date, :assigned_to_id)
+  end
+end
