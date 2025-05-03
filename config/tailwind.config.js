edbbf35496ad98199865002dadea22388f0cd9d0
Fabio Martins <fabio.martins@unila.edu.br>
2025-05-03 11:19:33 -0300

    chore(tailwind): update Tailwind CSS configuration

---
+++ config/tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ]
+} 
