ccd6041ee79e432770abefeabdfbf2590255a6b3
Fabio Martins <fabio.martins@unila.edu.br>
2025-05-05 13:40:52 -0300

    refactor: move Sidebar to improve responsiveness

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      }
     },
   },
   plugins: [
